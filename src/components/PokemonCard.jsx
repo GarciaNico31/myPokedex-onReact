e90366b2c1a47dc1f5bc79cac0f9d1e132fa794c
Nicolas GARCIA <garcianicopro@gmail.com>
2023-07-10 15:30:14 +0200

    Finish

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,5 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-const PokemonCard = () => {
-  const pokemon = pokemonList[0];
-
+const PokemonCard = (pokemon) => {
+  console.log(pokemon);
   return (
     <div>
       <figure>
@@ -20,6 +8,7 @@ const PokemonCard = () => {
       <figcaption>{pokemon.name}</figcaption>
     </div>
   );
+
 };
 
 export default PokemonCard;
