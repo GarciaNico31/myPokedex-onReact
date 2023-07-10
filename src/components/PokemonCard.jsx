1d7a47704bbf402ed31e7569baf17d7bae23cb24
Nicolas GARCIA <garcianicopro@gmail.com>
2023-07-10 14:25:13 +0200

    Finish

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+const PokemonCard = () =>{
+    return(
+        <div>
+        <figure>
+            <img src="src\assets\1.png" alt="bulbasaur" />
+        </figure>
+        <figcaption>bulbasaur</figcaption>
+        </div>
+    );
+};
+
+export default PokemonCard;
